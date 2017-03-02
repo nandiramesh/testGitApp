a89b8c3e65f9ba3c47a64d63515ddc90ccea6076
Ramesh <ramesh@Rameshs-Mac-mini.local>
2017-03-02 14:03:11 +0400

    this is first commit

--- TestGitFeatures/ViewController.swift
+++ TestGitFeatures/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    print("New Message")
   }
 
   override func didReceiveMemoryWarning() {
